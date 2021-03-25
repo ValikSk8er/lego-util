6b8d897d0fdb4ebf251a0c88a81b4f876915b5ab
Valentyn Chereshnevyi <valentyn_chereshnevyi@refinitiv.com>
2021-03-25 22:50:58 +0200

    init project

---
+++ src/main/java/com/vcherrylab/legoutil/LegoUtilApplication.java
@@ -0,0 +1,13 @@
+package com.vcherrylab.legoutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoUtilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcherrylab/legoutil/LegoUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcherrylab.legoutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
